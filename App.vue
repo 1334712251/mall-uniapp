390f5442e66e36894165b419e5d9dd51be793ae7
linhao <1334712251@qq.com>
2021-09-29 21:05:31 +0800

    提交结果

--- App.vue
+++ App.vue
@@ -14,4 +14,11 @@
 
 <style>
 	/*每个页面公共css */
+	/* 官方ui库 */
+	@import "/common/uni.css";
+	@import "animate.css";
+	/* 指定义图标库 */
+	@import "/common/icon.css";
+	/* 公共样式 */
+	@import "/common/common.css";
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,11 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
+		<!-- <image class="logo animate__flash" src="/static/logo.png"></image>
 		<view class="text-area">
 			<text class="title">{{title}}</text>
-		</view>
+		</view> -->
+
+		<view class="iconfont icon-wode"></view>
 	</view>
 </template>
 
